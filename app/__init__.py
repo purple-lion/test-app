7950a34126b256f0fd3f252d59253dd26a0faac4
Pyunghyuk Yoo <62975595+purple-lion@users.noreply.github.com>
2020-10-06 17:52:34 +0900

    feature: implement hello route

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+__version__ = '0.1.0'
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return f'Hello, {__version__}'
+
+    return app
